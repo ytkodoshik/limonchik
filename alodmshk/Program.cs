99649219845e31191a81833719cb01879fd0b1c1
Julia Lesyk <ytkodoshik@gmail.com>
2023-06-29 15:15:51 +0200

    alo

---
+++ alodmshk/Program.cs
@@ -0,0 +1,19 @@
+﻿const int X = 10;
+const int Y = 12;
+int sum = 0;
+
+if (X <= Y)
+{
+    for (int i = X; i <= Y; i++)
+    {
+        sum += i;
+    }
+}
+else
+{
+    for (int i = Y; i <= X; i++)
+    {
+        sum += i;
+    }
+}
+Console.WriteLine("Sum: " + sum);
